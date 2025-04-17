46d25dc1c5cdd3ca5fd17974e4fccb4b3687e772
Emmanuel Dillon <cesar.dillon1275@alumnos.udg.mx>
2025-04-17 13:57:06 -0600

    (feat)-> adding exception handling to support errors

---
+++ services/auth-service/src/main/java/com/dilon/filemanagerapp/dto/ExceptionResponse.java
@@ -0,0 +1,19 @@
+package com.dilon.filemanagerapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
+import java.util.Map;
+import java.util.Set;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public record ExceptionResponse(
+        Integer businessErrorCode,
+        String businessExceptionDescription,
+        String error,
+        Set<String> validateErrors,
+        Map<String, String> errors
+
+) {
+}
